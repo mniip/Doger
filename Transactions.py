502b0467f24accdd88d346cff4899a5c7c409005
mniip <mniip@mniip.com>
2014-06-05 04:49:36 +0400

    HIGHLY EXPERIMENTAL. Wallet renewal, restarts wallet from scratch and copies money over

--- Transactions.py
+++ Transactions.py
@@ -1,8 +1,9 @@
-import threading
+import threading, time, os
 import dogecoinrpc
-from dogecoinrpc.exceptions import InsufficientFunds
+from dogecoinrpc.exceptions import InsufficientFunds, WalletError
 
 conn = dogecoinrpc.connect_to_local()
+conn.proxy._transport.connection.timeout = 600
 lock = threading.Lock()
 
 class RpcLock(object):
@@ -49,8 +50,77 @@ def verify_address(addr):
 	inf = conn.validateaddress(addr)
 	return inf.isvalid
 
-"""
-def info():
-	inf = conn.getinfo()
-	return "Blocks: %i, Difficulty: %f, Total balance: %f" % (inf.blocks,inf.difficulty,inf.balance)
-"""
+def renew_wallet():
+	global conn
+	i = 0
+	try:
+		while True:
+			open(".dogecoin/renew_wallet.log." + str(i), "r").close()
+			i += 1
+	except IOError:
+		pass
+	conn.backupwallet(".dogecoin/wallet.bak." + str(i))
+	balances = {}
+	with open(".dogecoin/renew_wallet.log." + str(i), "w", 0) as f:
+		master = None
+		for user in conn.listaccounts():
+			balance = float(conn.getbalance(user, minconf = 4))
+			if balance:
+				balances[user] = balance
+				f.write("DUMP\tuser\t%d\t%s\n" % (balances[user], user.encode("utf8")))
+				for wallet in conn.getaddressesbyaccount(user):
+					f.write("DUMP\twallet\t%s\t%s\t%s\n" % (wallet, conn.dumpprivkey(wallet), user.encode("utf8")))
+					if user == "":
+						master = wallet
+		print(repr(balances))
+		if not master:
+			master = conn.getnewaddress("")
+		total = 0.0
+		for user in balances:
+			conn.move(user, "", balances[user])
+			total += balances[user]
+		masterkey = conn.dumpprivkey(master)
+		f.write("DUMP\tmaster\t%s\t%s\t%d\n" % (master, masterkey, total))
+		fee = 0
+		tx = None
+		while True:
+			try:
+				tx = conn.sendfrom("", master, total - fee, comment = 'Renewing wallet')
+				break
+			except WalletError, InsufficientFunds:
+				fee += 1
+		f.write("DUMP\tsend\t%d\t%d\t%s\n" % (total, -fee, tx.encode("utf8")))
+		time.sleep(20)
+		conn.stop()
+		conn.proxy._transport.connection.close()
+		try:
+			while True:
+				open(".dogecoin/dogecoind.pid", "r").close()
+				time.sleep(0.1)
+		except IOError:
+			pass
+		f.write("DUMP\tdaemon dead\n")
+		os.rename(".dogecoin/wallet.dat", ".dogecoin/wallet.bak.bak." + str(i))
+		os.system("./dogecoind")
+		f.write("RESTORE\tdaemon started\n")
+		while True:
+			try:
+				conn = dogecoinrpc.connect_to_local()
+				conn.proxy._transport.connection.timeout = 600
+				conn.getinfo()
+				break
+			except Exception as e:
+				print(repr(e))
+				conn.proxy._transport.connection.close()
+				time.sleep(1)
+		conn.importprivkey(masterkey, "", True)
+		f.write("RESTORE\tmaster\t%s\t%s\n" % (conn.getaddressesbyaccount("")[0], masterkey))
+		for user in balances:
+			try:
+				conn.move("", user, int(balances[user]))
+				f.write("RESTORE\tuser\t%d\t%s\n" % (int(conn.getbalance(user, minconf = 0)), user))
+			except:
+				f.write("RESTORE\tfail\t%d!=%d\t%s\n" % (int(conn.getbalance(user, minconf = 0)), balances[user], user))
+		if fee:
+			conn.move("@DONATIONS", "", fee)
+		f.write("DONE\n")
